a396d9dd988aa696688b653cf6eb5b77f882a8bd
vikingengineer <rakrutz@gmail.com>
2019-04-29 15:54:23 +0100

    Update FinanceTrackerYF.py

--- FinanceTrackerYF.py
+++ FinanceTrackerYF.py
@@ -1,22 +1,24 @@
+# 25 April 2019
+# Created by R. Krutz
 import pandas as pd
 import csv
 import stockHistoryYF as shyf
 
-'''
-a = pd.read_csv('OneSourceETFListTest.csv')
-#a =  pd.read_csv('OneSourceETFListTest.csv').to_dict()
+
+a = pd.read_csv('YOURFILEHERE.csv')
+
 print(a['ticker'][1])
 print(type(a['ticker'][1]))
 '''
-#stock = ['AAPL', 'GOOGL', 'GM', 'F']
-with open('OneSourceETFListTest.csv', 'r') as f:
+
+with open('YOURFILEHERE.csv', 'r') as f:
     reader = csv.reader(f)
     stock = list(reader)
 #print(stock)
 x = 1
 for value in stock:
     #print(stock[x])
-    shyf.stockHistoryYF(stock[x],'2019-04-08', '2019-04-20')
+    shyf.stockHistoryYF(stock[x],'YYYY-MM-DD', 'YYYY-MM-DD')
     #print('------------')
     x = x+1
 
